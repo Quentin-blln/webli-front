afc4382dc31c6a91e2a77f2afb683993b74dd3ac
Quentin BELLINO <quentin.bellino@C02GQ7QJQ6LR-MPL2FR.local>
2022-03-14 16:48:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import logo from './logo.svg';
+import * as React from 'react';
+
+import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import Popover from '@mui/material/Popover';
+
+
+function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  return (
+    <div className="App">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={handleClick}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              WEBLI
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Typography sx={{ p: 2 }}>Tools:</Typography>
+        <div><Button> Outils 1 </Button></div>
+        <div><Button>Outils 2</Button></div>
+        <div><Button>Outils 3</Button></div>
+      </Popover>
+    </div>
+  );
+}
+
+export default App;
