be9512f1cd89359aa87d5ade4e36fc3395fdc176
NemFR <49268689+NemFR@users.noreply.github.com>
2022-03-17 14:44:07 +0100

    BCK-09

--- src/components/tools.js
+++ src/components/tools.js
@@ -9,13 +9,29 @@ import Particles, { ISourceOptions } from "react-tsparticles";
 import particleOptions from './particlesOptions';
 import CircularProgress from '@mui/material/CircularProgress';
 
+import ReactTerminal from 'react-terminal-component';
+
+import {
+    EmulatorState, OutputFactory, Outputs
+} from 'javascript-terminal';
 
 
 const Tools = (toolIndexOb) => {
     const [johnPassToCrack, setJohnPassToCrack] = React.useState('')
     const [nmapIpOrDomain, setNmapIpOrDomain] = React.useState('')
     const [waitingForResponse, setWaitingForResponse] = React.useState(false)
+    const [backResponse, setBackResponse] = React.useState('Here will appear the kali command response')
+
     const toolIndex = toolIndexOb.toolIndex
+
+    const defaultState = EmulatorState.createEmpty();
+    const defaultOutputs = defaultState.getOutputs();
+
+    const newOutputs = Outputs.addRecord(
+        defaultOutputs, OutputFactory.makeTextOutput(backResponse)
+    );
+    let emulatorState = defaultState.setOutputs(newOutputs);
+
 
 
     const sendJohnHashToBack = (hash) => {
@@ -25,145 +41,154 @@ const Tools = (toolIndexOb) => {
             hash: hash
         }
         axios
-        .post('http://localhost:3001/john', obj)
-        .then(resp=>{
-            setWaitingForResponse(false)
-            alert('Réponse de John: ', resp.data)
-        })
-        .catch(err=>{
-            setWaitingForResponse(false)
-            alert(err)
-        })
+            .post('http://localhost:3001/john', obj)
+            .then(resp => {
+                setWaitingForResponse(false)
+                setBackResponse(resp.data)
+
+            })
+            .catch(err => {
+                setWaitingForResponse(false)
+                alert(err)
+            })
     }
 
     const sendIpOrDomainToBack = (ipOrDomain) => {
         setWaitingForResponse(true)
         axios
-        .post('http://localhost:3001/nmap', {ipOrDomain: ipOrDomain})
-        .then(resp=>{
-            setWaitingForResponse(false)
-            alert('Réponse de John: ', resp.data)
-        })
-        .catch(err=>{
-            alert(err)
-            setWaitingForResponse(false)
-        })
+            .post('http://localhost:3001/nmap', { ipOrDomain: ipOrDomain })
+            .then(resp => {
+                setWaitingForResponse(false)
+                setBackResponse(resp.data)
+            })
+            .catch(err => {
+                alert(err)
+                setWaitingForResponse(false)
+            })
     }
 
     console.log(toolIndex)
-    switch (toolIndex){
+    switch (toolIndex) {
         case 1:
-            return(
+            return (
                 <div>
-                    <Typography variant="h5" sx={{ m: 4}}>JOHN THE RIPPER</Typography>
+                    <Typography variant="h5" sx={{ m: 4 }}>JOHN THE RIPPER</Typography>
                     <Box
                         component="form"
-                        sx={{'& > :not(style)': { m: 1, width: '80ch' }}}
+                        sx={{ '& > :not(style)': { m: 1, width: '80ch' } }}
                         noValidate
                         autoComplete="off">
-                        <TextField id="outlined-basic" label="Password hash" variant="outlined" onChange={(event)=>{setJohnPassToCrack(event.target.value)}}/>
-                        <Button onClick={()=>{sendJohnHashToBack(johnPassToCrack)}}>Crack now</Button> 
+                        <TextField id="outlined-basic" label="Password hash" variant="outlined" onChange={(event) => { setJohnPassToCrack(event.target.value) }} />
+                        <Button onClick={() => { sendJohnHashToBack(johnPassToCrack) }}>Crack now</Button>
                     </Box>
+                    <div id="terminal"><ReactTerminal emulatorState={emulatorState} acceptInput={false} /></div>
                     {waitingForResponse
-                    ?
-                    <Box className='loading'>
-                        <CircularProgress />
-                    </Box>
-                    :null
+                        ?
+                        <Box className='loading'>
+                            <CircularProgress />
+                        </Box>
+                        : null
                     }
                 </div>
             )
         case 2:
-            return(
+            return (
                 <div>
-                    <Typography variant="h5" sx={{ m: 4}}>NMAP</Typography>
+                    <Typography variant="h5" sx={{ m: 4 }}>NMAP</Typography>
                     <Box
                         component="form"
-                        sx={{'& > :not(style)': { m: 1, width: '80ch' }}}
+                        sx={{ '& > :not(style)': { m: 1, width: '80ch' } }}
                         noValidate
                         autoComplete="off">
-                        <TextField id="outlined-basic" label="IP or Domain" variant="outlined" onChange={(event)=>{setNmapIpOrDomain(event.target.value)}}/>
-                        <Button onClick={()=>{sendIpOrDomainToBack(nmapIpOrDomain)}}> Send </Button> 
+                        <TextField id="outlined-basic" label="IP or Domain" variant="outlined" onChange={(event) => { setNmapIpOrDomain(event.target.value) }} />
+                        <Button onClick={() => {
+                            sendIpOrDomainToBack(nmapIpOrDomain);
+                        }}> Send </Button>
+
                     </Box>
+                    <div id="terminal"><ReactTerminal emulatorState={emulatorState} acceptInput={false} /></div>
                     {waitingForResponse
-                    ?
-                    <Box className='loading'>
-                        <CircularProgress />
-                    </Box>
-                    :null
+                        ?
+                        <Box className='loading'>
+                            <CircularProgress />
+                        </Box>
+                        : null
                     }
+
                 </div>
             )
         case 3:
-            return(
+            return (
                 <div>
-                    <Typography variant="h5" sx={{ m: 4}}>DDOS</Typography>
+                    <Typography variant="h5" sx={{ m: 4 }}>DDOS</Typography>
                     <Box
                         component="form"
-                        sx={{'& > :not(style)': { m: 1, width: '80ch' }}}
+                        sx={{ '& > :not(style)': { m: 1, width: '80ch' } }}
                         noValidate
                         autoComplete="off">
-                        <TextField id="outlined-basic" label="IP or Domain" variant="outlined" onChange={(event)=>{setJohnPassToCrack(event.target.value)}}/>
-                        <Button onClick={()=>{sendJohnHashToBack(johnPassToCrack)}}> Send </Button> 
+                        <TextField id="outlined-basic" label="IP or Domain" variant="outlined" onChange={(event) => { setJohnPassToCrack(event.target.value) }} />
+                        <Button onClick={() => { sendJohnHashToBack(johnPassToCrack) }}> Send </Button>
                     </Box>
                     {waitingForResponse
-                    ?
-                    <Box className='loading'>
-                        <CircularProgress />
-                    </Box>
-                    :null
+                        ?
+                        <Box className='loading'>
+                            <CircularProgress />
+                        </Box>
+                        : null
                     }
                 </div>
             )
         case 4:
-                return(
-                        <div>
-                            <Typography variant="h5" sx={{ m: 4}}>FIERCE</Typography>
-                            <Box
-                                component="form"
-                                sx={{'& > :not(style)': { m: 1, width: '80ch' }}}
-                                noValidate
-                                autoComplete="off">
-                                <TextField id="outlined-basic" label="Domain name" variant="outlined" onChange={(event)=>{setJohnPassToCrack(event.target.value)}}/>
-                                <Button onClick={()=>{sendJohnHashToBack(johnPassToCrack)}}> Send </Button> 
-                            </Box>
-                            {waitingForResponse
-                                ?
-                                <Box className='loading'>
-                                    <CircularProgress />
-                                </Box>
-                                :null
-                                }
-                        </div>
-                    )
+            return (
+                <div>
+                    <Typography variant="h5" sx={{ m: 4 }}>FIERCE</Typography>
+                    <Box
+                        component="form"
+                        sx={{ '& > :not(style)': { m: 1, width: '80ch' } }}
+                        noValidate
+                        autoComplete="off">
+                        <TextField id="outlined-basic" label="Domain name" variant="outlined" onChange={(event) => { setJohnPassToCrack(event.target.value) }} />
+                        <Button onClick={() => { sendJohnHashToBack(johnPassToCrack) }}> Send </Button>
+                    </Box>
+                    <div id="terminal"><ReactTerminal emulatorState={emulatorState} acceptInput={false} /></div>
+                    {waitingForResponse
+                        ?
+                        <Box className='loading'>
+                            <CircularProgress />
+                        </Box>
+                        : null
+                    }
+                </div>
+            )
         case 5:
-                return(
-                            <div>
-                                <Typography variant="h5" sx={{ m: 4}}>WPSCAN</Typography>
-                                <Box
-                                    component="form"
-                                    sx={{'& > :not(style)': { m: 1, width: '80ch' }}}
-                                    noValidate
-                                    autoComplete="off">
-                                    <TextField id="outlined-basic" label="Domain name" variant="outlined" onChange={(event)=>{setJohnPassToCrack(event.target.value)}}/>
-                                    <Button onClick={()=>{sendJohnHashToBack(johnPassToCrack)}}> Send </Button> 
-                                </Box>
-                                {waitingForResponse
-                                    ?
-                                    <Box className='loading'>
-                                        <CircularProgress />
-                                    </Box>
-                                    :null
-                                    }
-                            </div>
-                        )
+            return (
+                <div>
+                    <Typography variant="h5" sx={{ m: 4 }}>WPSCAN</Typography>
+                    <Box
+                        component="form"
+                        sx={{ '& > :not(style)': { m: 1, width: '80ch' } }}
+                        noValidate
+                        autoComplete="off">
+                        <TextField id="outlined-basic" label="Domain name" variant="outlined" onChange={(event) => { setJohnPassToCrack(event.target.value) }} />
+                        <Button onClick={() => { sendJohnHashToBack(johnPassToCrack) }}> Send </Button>
+                    </Box>
+                    <div id="terminal"><ReactTerminal emulatorState={emulatorState} acceptInput={false} /></div>
+                    {waitingForResponse
+                        ?
+                        <Box className='loading'>
+                            <CircularProgress />
+                        </Box>
+                        : null
+                    }
+                </div>
+            )
         default:
-            return(
-                <div style={{backgroundColor:'black', width:'100%', height:'100vh', transition: 'background-color 2s linear'}}>
-                    <img id="logo_homepage" src="../Webli_v2.png" alt="logo"/>
+            return (
+                <div style={{ backgroundColor: 'black', width: '100%', height: '100vh', transition: 'background-color 2s linear' }}>
+                    <img id="logo_homepage" src="../Webli_v2.png" alt="logo" />
                     <Particles options={particleOptions} />
                 </div>
-                )
+            )
     }
 }
 
